5135329ecc108aca2d93c256e10b33cca0486c2d
Anmol Ranjan <anmolranjan1@gmail.com>
2024-10-21 10:17:55 +0530

    Initial Commit

---
+++ FairTrip/FairTripApp.swift
@@ -0,0 +1,17 @@
+//
+//  FairTripApp.swift
+//  FairTrip
+//
+//  Created by Anmol Ranjan on 21/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FairTripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
